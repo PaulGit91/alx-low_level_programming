0a910882ab94c4b4bebcf06c1029f429e7f5fc0e
PaulGit91 <paula91github@gmail.com>
2022-10-18 09:16:01 -0700

    9e

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,6 +15,16 @@ void times_table(void)
 		{
 			_putchar(',');
 			_putchar(' ');
+
+			result = i * j;
+			if (result <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((result / 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
