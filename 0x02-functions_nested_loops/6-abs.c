071780f54d08401aca556ca9bcc4624de6bbf172
PaulGit91 <paula91github@gmail.com>
2022-10-18 07:47:08 -0700

    6a

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @x: The integer to be computed.
+ * Return: The absolute value of the integer.
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		_putchar(x);
+	}
+	else
+	{
+		_putchar(x);
+	}
+	return (0);
+}
