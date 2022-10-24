e34f7cb933c06fa50447c7a723370f5eebb50625
PaulGit91 <paula91github@gmail.com>
2022-10-24 07:38:18 -0700

    4a

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - prints string in reverse
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int x = 0;
+
+	while (s[x] != '\0')
+		x++;
+	for (x = x - 1; x >= 0; x--)
+		_putchar(s[x]);
+	_putchar('\n');
+}
