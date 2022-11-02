9aa72bcae73e6448dca7835854ec1b045732c71d
PaulGit91 <paula91github@gmail.com>
2022-11-02 06:26:15 -0700

    0b

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: The string that prints
+ * Return: nothing
+ */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
