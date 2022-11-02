6b31ae3e4a7c3b853d0a8ee209f431beae90ad9c
PaulGit91 <paula91github@gmail.com>
2022-11-02 08:11:19 -0700

    5a

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * _sqrt_recursion - find the square root of a given number.
+ * @n: number to find the square root of.
+ * Return: the sqrt as a Int.
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
