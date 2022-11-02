0c296ce162e47829aaa7143ecb1ea46615f21d16
PaulGit91 <paula91github@gmail.com>
2022-11-02 08:08:48 -0700

    6a

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n <= 1 && n >= 0)
+	{
+		return (n);
+	}
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * is_prime_number - determines if a number is prime number
+ * @n: input number
+ * Return: 1 if input is prime number
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
