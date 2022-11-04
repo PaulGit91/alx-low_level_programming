394f11f4be3c05b49f9d7cdb6211423913cdde4c
PaulGit91 <paula91github@gmail.com>
2022-11-04 16:40:34 -0700

    1r

---
+++ 0x09-static_libraries/_atoi.c
@@ -0,0 +1,6 @@
+#include "main.h"
+#include <stdlib.h>
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
---
+++ 0x09-static_libraries/_strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+char *_strncpy(char *dest, char *src, int n)
+{
+	int y;
+
+	y = 0;
+	while (y < n && src[y] != '\0')
+	{
+		dest[y] = src[y];
+		y++;
+	}
+	while (y < n)
+	{
+		dest[y] = '\0';
+		y++;
+	}
+	return (dest);
+}
