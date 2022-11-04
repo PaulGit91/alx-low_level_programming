493027a6153a1331653a0eab31a5585698b700f9
PaulGit91 <paula91github@gmail.com>
2022-11-04 08:33:24 -0700

    4a

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - adds positive numbers
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 1 if ERROR such as symbols that are not numbers,
+ * 0 if nothing is passed
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc < 1)
+	{
+		printf("0\n");
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += atoi(argv[i]);
+			printf("%d\n", sum);
+		}
+	}
+	return (0);
+}
