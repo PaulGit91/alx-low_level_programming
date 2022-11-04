62ad5bb5b66c708eba9965cc1996fda74fd8fe4c
PaulGit91 <paula91github@gmail.com>
2022-11-04 08:38:41 -0700

    4c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,25 +10,21 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	int sum = 0;
+	int i, sum = 0;
 
 	if (argc < 1)
-	{
-		printf("0\n");
-	}
+		return (0);
+
 	for (i = 1; i < argc; i++)
 	{
 		if (!atoi(argv[i]))
 		{
-			printf("Error\n");
+			printf("%s\n", "Error");
 			return (1);
 		}
-		else
-		{
-			sum += atoi(argv[i]);
-			printf("%d\n", sum);
-		}
+		sum += atoi(argv[i]);
 	}
+	printf("%d\n", sum);
+
 	return (0);
 }
