3dbcc7be4046219c62a710953900c403512af5f6
PaulGit91 <paula91github@gmail.com>
2022-11-04 07:29:34 -0700

    1b

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,14 +6,14 @@
  * @argv: argument vector
  * Return: Always 0
  */
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
 	int i;
 	(void)argv;
 
-	for (i = 0; i < argc; i++)
+	for (i = 0; i <= argc; i++)
 	{
-		printf("%d\n", argv[i]);
+		printf("%d\n", i -  2);
 	}
 	return (0);
 }
