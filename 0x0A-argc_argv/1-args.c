281584ad2e5deb9eefb88b07dcbcbc8e29f564d2
PaulGit91 <paula91github@gmail.com>
2022-11-04 07:25:52 -0700

    1a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv)
+{
+	int i;
+	(void)argv;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%d\n", argv[i]);
+	}
+	return (0);
+}
