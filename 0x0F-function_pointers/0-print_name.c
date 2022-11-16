5791526d78a61123d18be51b1c0207738ba3a853
PaulGit91 <paula91github@gmail.com>
2022-11-16 07:38:14 -0800

    0b

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,10 @@
 #include "function_pointers.h"
-#include <stdio.h>
-
+/**
+ * print_name - prints name
+ * @name: name to print
+ * @f: function to send the name to
+ * Return: nothing
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
