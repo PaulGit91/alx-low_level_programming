d764f59443f2d334f8a8a3d6ef63a16cf90fce5c
PaulGit91 <paula91github@gmail.com>
2022-11-17 03:49:52 -0800

    3a

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,84 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_char - prints the char character
+ * @list: the type
+ * Return: nothing
+ */
+void print_char(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+/**
+ * print_int - prints the int
+ * @list: the type
+ * Return: nothing
+ */
+void print_int(va_list list)
+{
+	printf("%i", va_arg(list, int));
+}
+/**
+ * print_float - prints the float
+ * @list: the type
+ * Return: nothing
+ */
+void print_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+/**
+ * print_string - prints the string
+ * @list: the type
+ * Return: nothing
+ */
+void print_string(va_list list)
+{
+	char *str;
+
+	str = va_arg(list, char *);
+	if (str == NULL)
+		str = "(nil)";
+
+	printf("%s", str);
+}
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to function
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	print_t types[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+
+        int x = 0, y = 0;
+	va_list list;
+	char *separator = "";
+
+	va_start(list, format);
+
+	while (format && format[x])
+	{
+		while (types[y].print)
+		{
+			if (format[x] == *types[y].print)
+			{
+				printf("%s", separator);
+				types[y].f(list);
+				separator = ", ";
+			}
+			y++;
+		}
+		y = 0;
+		x++;
+	}
+	printf("\n");
+	va_end(list);
+}
