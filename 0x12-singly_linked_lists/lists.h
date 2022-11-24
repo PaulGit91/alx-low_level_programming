578cc59ce978edcace5a4aa7162dcbe2de3a313b
PaulGit91 <paula91github@gmail.com>
2022-11-24 08:33:34 -0800

    0a

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,14 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+#include <unistd.h>
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif
