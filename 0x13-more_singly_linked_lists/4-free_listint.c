2b750f96388dc18aab056681015899564cb16870
PaulGit91 <paula91github@gmail.com>
2022-11-28 09:26:43 -0800

    4a

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - frees a list
+ * @head: the head of the list
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temporary;
+
+	while (head)
+	{
+		temporary = head->next;
+		free(head);
+		head = temporary;
+	}
+}
