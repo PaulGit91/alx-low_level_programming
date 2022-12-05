d2b4e9a0e8596b07d51b2128c4bce2da8c67c714
PaulGit91 <paula91github@gmail.com>
2022-12-05 03:43:56 -0800

    1d

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,36 +8,28 @@
  *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
-{
-	int len;
-	int fd, wr;
+{int o, w, len = 0;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		fd = open(filename, O_WRONLY | O_CREAT, 0600);
-		if (fd == -1)
+		for (len = 0; text_content[len];)
 		{
-			return (-1);
+			len++;
 		}
+	}
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+	{
 		return (-1);
 	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
-	{
-		return (-1);
-	}
-	for (len = 0; text_content[len]; len++)
-	{}
-		wr = write(fd, text_content, len);
-		if (wr == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-		close(fd);
+
+	close (o);
+
 	return (1);
 }
